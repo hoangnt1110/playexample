cf91531c3965385a97c2d1417603cdf072411462
hoangtien <hoang.tien@smarterhealth.sg>
2024-03-23 08:08:05 +0700

    11

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -14,7 +14,7 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
   /**
    * Create an Action to render an HTML page.
    *
-   * The configuration in the `routes` file means that this method
+   * The configuration in the `Routes` file means that this method
    * will be called when the application receives a `GET` request with
    * a path of `/`.
    */
